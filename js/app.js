ce213aacb3983be3b5b582050e29e6042774cff6
obamwonyi <developer.oduwa@gmail.com>
2022-04-03 09:36:09 +0100

    The section has been created dynamically and is no longer static

--- js/app.js
+++ js/app.js
@@ -4,8 +4,94 @@ The event listener below will fire when the page(window) loads this will run a c
 inside the call back function passed to it in the second parameter , this execution will simply create a navigation with all the neccessary class id and attributed needed for a responsive navigation bar. 
 
 */
+
+
+
+
+
+//precreating the section tag
+const section = document.createElement('section');
+//paragraph text for the sections 
+const sectionText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo 
+        donec enim diam vulputate. Aliquam faucibus purus in massa tempor nec. Est 
+        ultricies integer quis auctor elit sed vulputate mi sit. Dolor sed
+        viverra ipsum nunc aliquet bibendum enim. Vel quam elementum pulvinar
+        etiam non quam. Enim nunc faucibus a pellentesque sit amet porttitor
+        eget dolor. Massa tincidunt dui ut ornare lectus sit amet est. Consequat
+        semper viverra nam libero justo laoreet. Viverra vitae congue eu
+        consequat ac felis donec et. Ut tortor pretium viverra suspendisse
+        potenti nullam ac. Dignissim diam quis enim lobortis scelerisque
+        fermentum dui faucibus in. Commodo quis imperdiet massa tincidunt nunc
+        pulvinar. Magna fermentum iaculis eu non. Dignissim suspendisse in est
+        ante in nibh mauris cursus. Sed nisi lacus sed viverra tellus. Vivamus
+        arcu felis bibendum ut tristique et. Id diam vel quam elementum pulvinar
+        etiam non. In cursus turpis massa tincidunt dui. Ipsum suspendisse
+        ultrices gravida dictum fusce. Tellus orci ac auctor augue mauris augue
+        neque gravida. Amet consectetur adipiscing elit pellentesque habitant
+        morbi tristique senectus et. Pellentesque habitant morbi tristique
+        senectus et netus et. Vel eros donec ac odio tempor. In fermentum
+        posuere urna nec tincidunt praesent. In iaculis nunc sed augue lacus
+        viverra vitae congue eu. Nunc lobortis mattis aliquam faucibus purus in
+        massa tempor. Amet venenatis urna cursus eget nunc scelerisque viverra
+        mauris in. Etiam erat velit scelerisque in dictum non. Praesent
+        elementum facilisis leo vel fringilla est ullamcorper eget. `;
+
+//precreating the h1 element
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
+
+let sectionIdCount = 1; 
+
+//adding section to the body 
+for(i = 0; i < 4; i++) 
+{
+  section.setAttribute('id','section'+sectionIdCount);
+  document.body
+    .appendChild(section.cloneNode());
+  sectionIdCount++;
+}
+
+const sectionAll = document.querySelectorAll("section");
+
+let sectionHeaderCount = 1;
+//adding h1 and p to each section 
+for(eachSection of sectionAll)
+{
+   eachSection.appendChild(h1.cloneNode()).insertAdjacentText('afterbegin',"Section"+sectionHeaderCount);
+   eachSection.appendChild(p.cloneNode()).insertAdjacentText('afterbegin',sectionText);
+   sectionHeaderCount++;
+}
+
+
+
+//creating footer and its child nodes -----------------------------/
+const footer = document.createElement('footer');
+let anchor = document.createElement('a');
+document.body.appendChild(footer);
+let footerNode = document.querySelector('footer');
+footerNode.classList.add('footer');
+let footerP = footerNode.appendChild(p);
+let footerAnchor = footerP.appendChild(anchor);
+footerAnchor.setAttribute('href','copyright');
+footerAnchor.insertAdjacentHTML("afterbegin", "&#169; udacity landing page");
+
+
+
+
+
+
+
+
+
+
 window.addEventListener("load",() => 
 {
+
+
+
+
+  
     
     //precreating the header tag
     const header =  document.createElement('header');
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # The landing page project
 
 * The navigation was created dynamically as instructed. 
-* The code is properly commented to provide information for every code written
-* The navigation to section by scrolling was implemented 
+* The code is properly commented to provide information for every code written.
+* The navigation to section by scrolling was implemented .
+* Section has been added dynamically .
