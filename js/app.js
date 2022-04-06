ee6eb7d37e4a34a5da239567c70d3654b7eedb2d
obamwonyi <developer.oduwa@gmail.com>
2022-04-06 13:26:15 +0100

    The code has been re written to meet requirment

--- js/app.js
+++ js/app.js
@@ -1,73 +1,71 @@
 /* ------------------------------creating the navigation ------------------------*/
 /* precreating all tags needed to be created in the dom */
 
-const header = document.createElement('header');
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const li = document.createElement('li');
-const section = document.createElement('section');
-const h1 = document.createElement('h1');
-const p = document.createElement('p');
-const a = document.createElement('a');
-const footer = document.createElement('footer');
+const header = document.createElement("header");
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+const p = document.createElement("p");
+const a = document.createElement("a");
+const footer = document.createElement("footer");
 
 /* building the navigation -----------------------------------*/
 
 //adding the header node to the body node as a child
 const createdHeader = document.body.appendChild(header);
 /* working with nav in header ------------------------*/
-//adding the nav node as a child to the header node 
+//adding the nav node as a child to the header node
 const createdNav = createdHeader.appendChild(nav);
-//adding a class to the nav node 
-createdNav.classList.add('navbar');
-//adding an anchor node to the nav node 
+//adding a class to the nav node
+createdNav.classList.add("navbar");
+//adding an anchor node to the nav node
 const createdAnchor = createdNav.appendChild(a);
-//adding a class to the anchor node in the nav node 
-createdAnchor.classList.add('nav-branding');
+//adding a class to the anchor node in the nav node
+createdAnchor.classList.add("nav-branding");
 createdAnchor.appendChild(document.createTextNode("Landing Page."));
 /* working with ul in the nav node ------------------------*/
 //adding the ul node as a child to the header node
 const createdUl = createdNav.appendChild(ul);
-//adding a class to the ul node 
-createdUl.classList.add('nav-menu');
+//adding a class to the ul node
+createdUl.classList.add("nav-menu");
 /*adding four li node to the ul node as well as adding thier classes
 and also adding an anchor to the li node as well as adding classed to the 
 anchor */
-// creating a new anchor node 
-const anchor = document.createElement('a');
-for(i = 1; i < 5; i++)
-{
-  //adding an li node to the ul node as a child 
+// creating a new anchor node
+const anchor = document.createElement("a");
+for (i = 1; i < 5; i++) {
+  //adding an li node to the ul node as a child
   let createdLi = createdUl.appendChild(li.cloneNode());
-  //adding a class to the li node 
-  createdLi.classList.add('nav-item');
+  //adding a class to the li node
+  createdLi.classList.add("nav-item");
   //adding an anchor node to the li node
-  //adding a class to each anchor node in the li node 
-  anchor.classList.add('nav-link');
+  //adding a class to each anchor node in the li node
+  anchor.classList.add("nav-link");
   //adding an it attribute to each anchor
-  anchor.setAttribute('id',i)
+  anchor.setAttribute("id", i);
   //adding an href attribute to each anchor
-  anchor.setAttribute('href','#section'+i);
-  //finally appending the anchor to the li node 
+  anchor.setAttribute("href", "#section" + i);
+  //finally appending the anchor to the li node
   let createdA = createdLi.appendChild(anchor.cloneNode());
   //adding an event listener to the anchor tags ----------------
-  createdA.addEventListener('click',(e) => {
-    //preventing the default action of the anchor tag 
-    e.preventDefault(); 
-    //fetching the anchors tag by the href attribute 
+  createdA.addEventListener("click", (e) => {
+    //preventing the default action of the anchor tag
+    e.preventDefault();
+    //fetching the anchors tag by the href attribute
     //const hrefAnchor  = document.querySelector('href');
-    anchorAttribute = e.target.getAttribute('href');
+    anchorAttribute = e.target.getAttribute("href");
     //this implements the scrolling to the sections
     document.querySelector(anchorAttribute).scrollIntoView({
-      behavior: "smooth"
+      behavior: "smooth",
     });
-    
-  })//----------------------------event ending ------------------
-  createdA.appendChild(document.createTextNode("section"+i));
+  }); //----------------------------event ending ------------------
+  createdA.appendChild(document.createTextNode("section" + i));
 }
 /* ---------------creating the sections ------------------------------*/
-//The sections will be created along with a scroll event to fire 
-//when the view is at the top most 
+//The sections will be created along with a scroll event to fire
+//when the view is at the top most
 //paragraph text node ---------------
 pTextNode = document.createTextNode(`
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -96,30 +94,59 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         elementum facilisis leo vel fringilla est ullamcorper eget.
 `);
 
-//creating the section node 
-for(i = 1; i < 5; i++)
-{
-  //adding the section node to the body node 
+//creating the section node
+for (i = 1; i < 5; i++) {
+  //adding the section node to the body node
   createdSection = document.body.appendChild(section.cloneNode());
-  //adding an attribute to the section 
-  createdSection.setAttribute('id',"section"+i);
-  //adding an h1 node to the section node 
+  //adding an attribute to the section
+  createdSection.setAttribute("id", "section" + i);
+  //adding an h1 node to the section node
   createdH1 = createdSection.appendChild(h1.cloneNode());
-  //adding an attribute to the h1 node 
-  createdH1.setAttribute('id',"header"+i);
-  //adding a text node to the h1 node 
-  createdH1.appendChild(document.createTextNode("section"+i));
-  //adding a paragraph node to the section node 
+  //adding an attribute to the h1 node
+  createdH1.setAttribute("id", "header" + i);
+  //adding a text node to the h1 node
+  createdH1.appendChild(document.createTextNode("section" + i));
+  //adding a paragraph node to the section node
   createdP = createdSection.appendChild(p.cloneNode());
-  createdP.setAttribute('id','p'+i);
-  //adding a text node to the paragraph node 
+  createdP.setAttribute("id", "p" + i);
+  //adding a text node to the paragraph node
   createdP.appendChild(pTextNode.cloneNode());
 }
 
-window.addEventListener('scroll',(e) => {
+
+//creating footer and its child nodes -----------------------------/
+let footerA = document.createElement("a");
+document.body.appendChild(footer);
+let footerNode = document.querySelector("footer");
+footerNode.classList.add("footer");
+let footerP = footerNode.appendChild(p);
+let footerAnchor = footerP.appendChild(footerA);
+footerAnchor.setAttribute("href", "copyright");
+footerAnchor.insertAdjacentHTML("afterbegin", "&#169; udacity landing page");
+
+
+
+/* 
+checking for a scroll in the windows and firing and event 
+to create and element highlighting for a particular section 
+when it's at a particular view in the window
+*/
+window.addEventListener("scroll", (e) => {
   e.preventDefault();
-  selSections = document.querySelector('section1');
-
-
-
-})
+  //fetch all the section when scrolling the page
+  allSection = document.querySelectorAll("section");
+  //looping through each section
+  allSection.forEach((aSection) => {
+    //fetching the dimension for each section
+    aSecDimension = aSection.getBoundingClientRect();
+    //if the section top is 0 px away from the top of the window
+    //or is a negative pixes distance from the top but its bottom
+    // is still in view highlight the section when scrolling from the top
+    // if not remove the highlighting from the section
+    if (aSecDimension.top <= 0 && aSecDimension.bottom > 0) {
+      aSection.classList.add("activeSection");
+    } else {
+      aSection.classList.remove("activeSection");
+    }
+  });
+});
--- readme.md
+++ readme.md
@@ -1,6 +1,23 @@
 # The landing page project
 
-* The navigation was created dynamically as instructed. 
-* The code is properly commented to provide information for every code written.
-* The navigation to section by scrolling was implemented .
-* Section has been added dynamically .
+* The navigation of the project is created dynamically .
+* The section of the project is also created dynamically .
+* Each section in the page is highlighted when scrolled or navigated(with the navigation links) into view .
+* Each code is Commented properly for a good guide on there function in the project. 
+* All the child in the body node are created dynamically . 
+
+## Note : 
+The navigation had extra nodes like the header, nav and anchor this is to follow a good css and html practice of creating a good  navigation structure, dispite this the gradual build up with the ul was still implemented . 
+When the section is highlighted it pops up with a box shadow
+and a background image made from three gradients.
+
+When tested in my platform the website is responsive for 
+the following platform : 
+* Mobile 
+* Tabs 
+* Desktop/Laptop
+
+The responsiveness was done with actual device test and not 
+by simply shrinking the web window, as shrinking the window does not give the exact reponsiveness. 
+
+### By, Destiny Obamwonyi
